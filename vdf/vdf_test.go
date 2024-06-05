fe3419b8984e2830776c990546ca9b23c6b79d9b
Agost Biro <agostbiro@gmail.com>
2024-06-05 23:19:15 +0200

    Add nekryptology compatibility tests

--- vdf/vdf_test.go
+++ vdf/vdf_test.go
@@ -1,17 +1,55 @@
 package vdf_test
 
 import (
+	"golang.org/x/crypto/sha3"
 	"source.quilibrium.com/quilibrium/cermonyclient/vdf"
+	nekrovdf "source.quilibrium.com/quilibrium/monorepo/nekryptology/pkg/vdf"
 	"testing"
 )
 
 const intSizeBits = uint16(2048)
-const difficulty = uint64(10000)
+
+func getChallenge(seed string) [32]byte {
+	return sha3.Sum256([]byte(seed))
+}
 
 func TestProveVerify(t *testing.T) {
-	solution := vdf.WesolowskiSolve(intSizeBits, []byte{0x01, 0x02, 0x03}, difficulty)
-	isOk := vdf.WesolowskiVerify(intSizeBits, []byte{0x01, 0x02, 0x03}, difficulty, solution)
+	difficulty := uint64(10000)
+	challenge := getChallenge("TestProveVerify")
+	solution := vdf.WesolowskiSolve(intSizeBits, challenge[:], difficulty)
+	isOk := vdf.WesolowskiVerify(intSizeBits, challenge[:], difficulty, solution)
 	if !isOk {
-		t.Errorf("WesolowskiVerify failed")
+		t.Fatalf("Verification failed")
+	}
+}
+
+func TestProveRustVerifyNekro(t *testing.T) {
+	difficulty := 100
+	challenge := getChallenge("TestProveRustVerifyNekro")
+
+	for i := 0; i < 100; i++ {
+		solution := vdf.WesolowskiSolve(intSizeBits, challenge[:], uint64(difficulty))
+		nekroVdf := nekrovdf.New(uint32(difficulty), challenge)
+		isOk := nekroVdf.Verify([516]byte(solution))
+		if !isOk {
+			t.Fatalf("Verification failed")
+		}
+		challenge = sha3.Sum256(solution)
+	}
+}
+
+func TestProveNekroVerifyRust(t *testing.T) {
+	difficulty := 100
+	challenge := getChallenge("TestProveNekroVerifyRust")
+
+	for i := 0; i < 100; i++ {
+		nekroVdf := nekrovdf.New(uint32(difficulty), challenge)
+		nekroVdf.Execute()
+		proof := nekroVdf.GetOutput()
+		isOk := vdf.WesolowskiVerify(intSizeBits, challenge[:], uint64(difficulty), proof[:])
+		if !isOk {
+			t.Fatalf("Verification failed")
+		}
+		challenge = sha3.Sum256(proof[:])
 	}
 }
